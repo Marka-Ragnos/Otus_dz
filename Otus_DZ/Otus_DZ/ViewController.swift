1e2b96f3e086ca430025d38a2d3cf7df8f79a425
Alexander S <dunkan-1@yandex.ru>
2023-10-21 16:44:16 +0300

    feat: create project

---
+++ Otus_DZ/Otus_DZ/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Otus_DZ
+//
+//  Created by Alexander Sukhodolov on 21.10.2023.
+//
+
+import UIKit
+
+class UserDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
